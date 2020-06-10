eef1a994c83142b0082a8614b677697bb1ea7a2a
chowon0125 <chowon0125@gmail.com>
2020-06-10 15:50:07 +0900

    15:49

---
+++ race/Player.java
@@ -0,0 +1,29 @@
+package race;
+
+public class Player {
+
+	private String name;
+	private int record;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setRec(int record) {
+		this.record = record;
+	}
+	public int getRec() {
+		return this.record;
+	}
+	
+	
+	void result() {
+	
+		System.out.println("이름 :" + name + "기록 :" + record);
+		
+	
+	}
+	}
---
+++ race/PlayerApp.java
@@ -0,0 +1,27 @@
+package race;
+import java.util.Scanner;
+public class PlayerApp {
+	public static void main(String[] args) {
+	
+		Scanner scanner = new Scanner(System.in);
+		Player[] players = new Player[3];
+		
+		for(int i=0; i<players.length; i++) {
+			
+			Player player = new Player();
+			System.out.println("이름 :");
+			player.setName(scanner.next());
+			System.out.println("기록 :");
+			player.setRec(scanner.nextInt());
+		
+			players[i] = player;
+	
+		}
+		
+		for(int i=0; i<players.length; i++) {
+			
+			players[i].result();
+		}
+		
+}
+}
