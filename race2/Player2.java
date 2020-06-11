b74a03d254ce1c5cdc78cb8889a0aa82870a25c4
chowon0125 <chowon0125@gmail.com>
2020-06-11 10:05:24 +0900

    1004

--- race2/Player2.java
+++ race2/Player2.java
@@ -2,4 +2,25 @@ package race2;
 
 public class Player2 {
 
+	private String name;
+	private int record;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setRecord(int record) {
+		this.record = record;
+	}
+	public int getRecord() {
+		return this.record;
+	}
+	
+	public void result() {
+		System.out.println("이름 : " + name + "기록 : " + record);
+	}
 }
+
--- race2/PlayerApp2.java
+++ race2/PlayerApp2.java
@@ -1,5 +1,39 @@
 package race2;
 
+import java.util.Scanner;
+
 public class PlayerApp2 {
 
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		Player2[] list = new Player2[3];
+		
+		for(int i=0; i<list.length; i++) {
+			
+			Player2 racer = new Player2();
+			
+			System.out.println("이름 : ");
+			racer.setName(scanner.next());
+			System.out.println("기록 : ");
+			racer.setRecord(scanner.nextInt());
+			
+			list[i] = racer;
+		}
+		
+		for(int i=0; i<list.length; i++) {
+			list[i].result();
+		}
+		
+		Player2 winner = new Player2();
+		winner = list[0];
+		for(int i = 1; i<list.length; i++) {
+			if(list[i].getRecord() < winner.getRecord()) {
+				winner = list[i];
+			}
+		}
+		
+		System.out.println("우승자는 "+winner.getName()+"입니다.");
+		
+	}
 }
