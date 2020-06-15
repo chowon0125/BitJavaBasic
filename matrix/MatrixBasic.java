76f9c95f8a2d96a6bad429f81a1293b664e176fb
chowon0125 <chowon0125@gmail.com>
2020-06-15 18:03:59 +0900

    200615_1803

--- matrix/MatrixBasic.java
+++ matrix/MatrixBasic.java
@@ -3,18 +3,25 @@ package matrix;
 public class MatrixBasic {
 	public static void main(String[] args) {
 		
-		int[][] mtx = new int[5][5];
-		for(int i = 0;i < mtx.length;i++) {
-			for(int j = 0;j < mtx[i].length;j++) {
-				//System.out.printf("[%d, %d]", i,j);
-				mtx[i][j] = (5*i) + j;
-						 
+		
+		int[][] mtx2 = new int[3][];
+		
+		int count2 = 1;
+		
+		mtx2[0] = new int[3];
+		mtx2[1] = new int[2];
+		mtx2[2] = new int[5];
+		
+		for(int i = 0;i<mtx2.length;i++) {
+			for(int j = 0;j<mtx2[i].length;j++) {
+				mtx2[i][j] = count2++;
 			}
 		}
-		for(int i = 0;i < mtx.length;i++) {
+		
+		for(int i = 0;i < mtx2.length;i++) {
 			System.out.println("");
-			for(int j = 0;j < mtx[i].length;j++) {
-				System.out.printf(mtx[i][j]+"\t");
+			for(int j = 0;j < mtx2[i].length;j++) {
+				System.out.printf(mtx2[i][j]+"\t");
 			}	
 		}
 	}
