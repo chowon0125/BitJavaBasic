e91464893f3be99cf4df97aee194dcd99ba069d3
chowon0125 <chowon0125@gmail.com>
2020-06-17 17:32:29 +0900

    200617_1732

--- matrix/LottoApp2.java
+++ matrix/LottoApp2.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 
 class Lotto2{
 	
-	private int countLimit;
+	private int countLimit; 
+	private int lottoCount;
+	private int[] lotto;
+	private int[][] lottos;
 	
 	public void setCountLimit(int countLimit) {
 		this.countLimit = countLimit;
@@ -13,25 +16,62 @@ class Lotto2{
 	public int getCountLimit() {
 		return countLimit;
 	}
-	public int lottoCount(int money) {
-		int count;
-		if(money<1000) {System.out.println("금액이 부족합니다."); count = 0;}
-		else {
-			if(money > countLimit * 1000) count = countLimit;
-			else count = money / 1000;
+	
+	public void setLottos() {
+		lotto = new int[6];
+		lottos = new int[lottoCount][6];
+		for(int i=0; i<lottoCount; i++) {
+			for(int j=0; j<6; j++) {
+				int num = (int)(Math.random()*45)+1;
+				while(!isDuplication(num)) {
+					lotto[j] = num;
+					break;
+				}
+			}
+			sort(lotto);
+			System.arraycopy(lotto, 0, lottos[i], 0, lotto.length);
+			//array를 matrix에 한 줄씩 입력하는 방법.
 		}
-		return count;
+	}
+	public int[][] getLottos() {
+		setLottos();
+		return lottos;
+	}
+	
+	public void sort(int[] arr) {
+		int temp;
+		for(int i=0; i<arr.length-1; i++) {
+			for(int j = i+1; j<arr.length; j++) {
+				if(arr[i] > arr[j]) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+	this.lotto = arr;
+	}
+	
+	public int lottoCount(int money) {
+		int lottoCount;
+		if(money<1000) {System.out.println("금액이 부족합니다."); lottoCount = 0;}
+		else {
+			if(money > countLimit * 1000) lottoCount = countLimit;
+			else lottoCount = money / 1000;
+		}
+		this.lottoCount = lottoCount;
+		return lottoCount;
 	}
 	
 	
-	public boolean isDuplication(int[] lotto, int num) {
+	public boolean isDuplication(int num) {
 		boolean ok = false;
 		for(int i = 0; i< lotto.length; i++) {
 			if(lotto[i] == num) ok = true;
 		}
 		return ok;
 	}
-	public void lottoSelect(Random random, int[] lotto, Lotto2 instance) {
+	/*public void lottoSelect(Random random, int[] lotto, Lotto2 instance) {
 		for(int i = 0; i< lotto.length; i++) {
 			int temp = random.nextInt(45)+1;
 			System.out.printf("뽑힌 값: %d\n", temp);
@@ -42,19 +82,26 @@ class Lotto2{
 			lotto[i] = temp;
 		}
 	}
-	public void buyLotto(Random random, Scanner scanner, Lotto2 instance, int[] lotto) {
+	*/
+	 public void buyLotto(Scanner scanner, Lotto2 instance) {
 		System.out.println("얼마치를 구입합니까?");
 		System.out.printf("로또 %d장을 구매합니다.%n",instance.lottoCount(scanner.nextInt()));
-		instance.lottoSelect(random, lotto, instance);
+		int[][] lottos = instance.getLottos();
+		for(int i=0; i<lottos.length;i++) {
+			System.out.print((i+1)+"번째 결과 :   ");
+			for(int j=0; j<lottos[i].length; j++) {
+				System.out.print(lottos[i][j]+"\t");
+			}
+			System.out.println();
+		}
 	}
+	
 }
 public class LottoApp2 {
 	
 	public static void main(String[] args) {
-		Random random = new Random();
 		Scanner scanner = new Scanner(System.in);
 		Lotto2 instance = new Lotto2();
-		int[] lotto = new int[6];
 		System.out.println("구매한도 설정");
 		instance.setCountLimit(scanner.nextInt());
 		System.out.printf("설정된 구매한도  %d장%n",instance.getCountLimit());
@@ -63,7 +110,7 @@ public class LottoApp2 {
 			switch(scanner.nextInt()) {
 			case 0 : System.out.println("종료"); return;
 			case 1 : System.out.println("로또 구입");
-			instance.buyLotto(random, scanner, instance, lotto);
+			instance.buyLotto(scanner, instance);
 			break;
 			}
 		}
---
+++ solve/MoneyUnit.java
@@ -0,0 +1,94 @@
+package solve;
+
+import java.util.Scanner;
+/*
+ * * 금액을 입력받은 후 우리나라 화폐종류별로 해당 갯수를 표기하는 프로그램
+  * [요구사항] 금융업을 하는 고객사로부터 프로그램 개발요청이 들어왔습니다.
+  * 금액을 입력받은 후 우리나라 화폐종류별로 해당 갯수를 표기하는 프로그램입니다.
+  * 예를들어, 125,520 원을 입력하면 화면에 이렇게 보이게 하면 됩니다.
+  * 표시하고 10원미만은 절삭
+   ******************************************************
+     요청금액 : 126520 원
+     5만원 : 2매
+     1만원 : 2매
+     5천원 : 1매
+     1천원 : 1매
+     5백원 : 1개
+     백원 : 0개
+     오십원 : 0개
+     십원 : 2개
+   ********************************************************
+ */
+class Money{
+	private int temp, money;
+	int[] arr = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
+	int[] result = new int[8];
+		
+	public void calculate(int money) {
+		this.money = money;
+		temp = money;
+		for(int i=0; i<arr.length; i++) {
+			result[i] = temp / arr[i];
+			temp = temp - (arr[i]*result[i]);
+		}
+	}
+	
+	public void print() {
+		System.out.printf("****************************%n요청금액 : %d원%n5만원: %d매%n1만원: %d매%n5천원: %d매%n1천원: %d매%n"
+				+ "500원: %d개%n백원: %d개%n오십원: %d개%n십원: %d개%n****************************%n"
+				,money, result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7]);
+	}
+}
+
+public class MoneyUnit {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Money instance = new Money();
+		System.out.println("금액을 입력하세요.");
+		instance.calculate(scanner.nextInt());
+		instance.print();
+	}
+}
+
+/*m50k = money / 50000;
+		
+		temp = money - (m50k*50000);
+		
+		m10k = temp / 10000;
+		
+		temp = temp - (m10k*10000);
+		
+		m5k = temp / 5000;
+		
+		temp = temp - (m5k*5000);
+		
+		m1k = temp / 1000;
+		
+		temp = temp - (m1k*1000);
+		
+		m500 = temp / 500;
+		
+		temp = temp - (m500*500);
+		
+		m100 = temp / 100;
+		
+		temp = temp - (m100*100);
+		
+		m50 = temp / 50;
+		
+		temp = temp - (m50*50);
+		
+		m10 = temp / 10;
+		
+		temp = temp - (m10*10);
+		
+		
+		
+		System.out.printf("****************************%n요청금액 : %d원%n5만원: %d매%n1만원: %d매%n5천원: %d매%n1천원: %d매%n"
+				+ "500원: %d개%n백원: %d개%n오십원: %d개%n십원: %d개%n****************************%n"
+							,money, m50k, m10k, m5k, m1k, m500, m100, m50, m10);
+							
+ */
+
+
+
